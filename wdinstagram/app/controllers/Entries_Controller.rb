edf42c95ca1ea67069aae34b385734f20321b2cc
Edward Park <parkes.edward@gmail.com>
2015-07-27 15:21:32 -0400

    added create and show actions#views

--- wdinstagram/app/controllers/Entries_Controller.rb
+++ wdinstagram/app/controllers/Entries_Controller.rb
@@ -3,4 +3,23 @@ class EntriesController < ApplicationController
     @entries = Entry.all.order(:id).reverse
   end
 
+  def new
+    @entry = Entry.new
+  end
+
+  def create
+    binding.pry
+    @entry = Entry.create( entry_params )
+    redirect_to @entry #remember it will just call url_for on this.
+  end
+
+  def show
+    @entry = Entry.find(params[:id])
+  end
+
+  private
+
+  def entry_params #forgot strong params
+    params.require(:entry).permit(:author, :photo_url,:date_taken)
+  end
 end
